61f726a2e13fe0f33a9030f8ce00d6f266fd035c
Mohammad Javidi <m.javidii@yahoo.com>
2020-06-20 14:30:34 +0430

    Reorganize file
    All file

--- README.md
+++
@@ -1,1 +0,0 @@
-#English isn't my first language, so please excuse any mistakes
--- script.js
+++
@@ -1,8 +0,0 @@
-function once_alert() {
-	var alerted = localStorage.getItem('alerted') || '';
-	if (alerted != 'yes') {
-		var txt = "Welcome - English isn\'t my first language, so please excuse any mistakes - and say to me - Thanks";
-		alert(txt);
-		localStorage.setItem('alerted','yes');
-	}
-}
