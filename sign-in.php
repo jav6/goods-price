dfd4a40d340d61adcaac59e9d61d3560a2b10c9c
Mohammad Javidi <m.javidii@yahoo.com>
2020-06-30 22:19:05 +0430

    Add a form for sign in

---
+++ sign-in.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sign In</title>
+</head>
+<body>
+	<center>
+	<form action="" method="_GET">
+		<table>
+			<tr>
+				<td>User name :</td>
+				<td><input type="text" name="username" /></td>
+			</tr>
+			<tr>
+				<td>Password :</td>
+				<td><input type="text" name="password" /></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="Sign In" /></td>
+			</tr>
+		</table>
+	</form>
+</center>
+</body>
+</html>
