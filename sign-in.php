4cb195807ef2b071019a0f475c87438ba2a4faab
Mohammad Javidi <m.javidii@yahoo.com>
2020-07-11 21:06:45 +0430

    fix login problem

--- sign-in.php
+++ sign-in.php
@@ -2,35 +2,22 @@
 $title = "Admin";
 include 'include/db_con.php';
 include 'include/function.php';
-//session_start();
-//$is_admin = null;
-//if (isset($_SESSION["admin"])){
-//$sql_query_check_token = "SELECT token from user WHERE token = '{$_SESSION["admin"]}';";
-//$sql_qu_tok_result = mysqli_query($db_con, $sql_query_check_token);
-//$sql_qu_tok_result_num = mysqli_num_rows($sql_qu_tok_result);
-//if ($sql_qu_tok_result_num > 0){
-//    while ($row = mysqli_fetch_assoc($sql_qu_tok_result)){
-//        if ($row["token"] === $_SESSION["admin"]){
-//            $is_admin = true;
+session_start();
+$is_admin = null;
+if (isset($_SESSION["admin"])){
+$sql_query_check_token = "SELECT token from user WHERE token = '{$_SESSION["admin"]}';";
+$sql_qu_tok_result = mysqli_query($db_con, $sql_query_check_token);
+$sql_qu_tok_result_num = mysqli_num_rows($sql_qu_tok_result);
+if ($sql_qu_tok_result_num > 0){
+    while ($row = mysqli_fetch_assoc($sql_qu_tok_result)){
+        if ($row["token"] === $_SESSION["admin"]){
+            $is_admin = true;
 ?>
 <?php
-//$is_admin_ex_inc = include 'include/layout/htmlstart.php';
-$is_admin_run = "
-    <center>
-	<img src='media/safety.png' width=\"100\" height=\"100\"><br>
-        <p>You are Admin</p><br>
-        <a href=\"include/do-clear-session.php\">Log Out</a>
-    </center>
-    ";
-//$is_admin_ex_inc .= include 'include/layout/footer.php';
-//$is_admin_ex_inc .= include 'include/layout/htmlend.php';
-?>
-<?php
-check_access($is_admin_run);
-//        }
-//    }
-//}
-//}
+        }
+    }
+}
+}
 
 if ($is_admin === null){
 if (isset($_GET["submit"])) {
@@ -108,6 +95,8 @@ if (isset($username) && isset($password)) {
 </body>
 </html>
 <?php
+}else{
+    echo "you Are Admin";
 }
 $db_con->close();
 ?>
