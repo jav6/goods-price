e3059c12bfefc3d7f0b02720434a309c7a6b7fcc
Mohammad Javidi <m.javidii@yahoo.com>
2020-07-19 07:45:57 +0430

    change user database

--- include/db_con.php
+++ include/db_con.php
@@ -1,7 +1,7 @@
 <?php
 // create database connection
 $db_host = "localhost";
-$db_user = "root";
+$db_user = "admin";
 $db_pass = "toor";
 $db_name = "goods-price";
 $db_con = new mysqli($db_host, $db_user, $db_pass, $db_name) ;
