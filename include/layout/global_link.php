e580b118053bd4a348a8a1255b00f562c2c7d224
Mohammad Javidi <m.javidii@yahoo.com>
2020-07-23 19:12:15 +0430

    add global link icon

---
+++ include/layout/global_link.php
@@ -0,0 +1,7 @@
+<br>
+<br>
+<a href="/" ><img src="media/search.png"></a>
+&nbsp;
+<a href="do-insert.php" ><img src="media/add.png"></a>
+&nbsp;
+<a href="sign-in.php" ><img src="media/authentication.png"></a>
