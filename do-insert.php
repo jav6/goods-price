225e002ad100449a2b837452aab7516bf53f7b8b
Mohammad Javidi <m.javidii@yahoo.com>
2020-07-24 18:48:14 +0430

    repair insert-data php file and refactor the code

--- do-insert.php
+++ do-insert.php
@@ -1,28 +1,33 @@
 <?php
+//page info tag
+$title = "Insert Data";
+$header = "For insert goods";
 
-include 'db_con.php';
+//include database connection
+include 'include/db_con.php';
+//include function
+include 'include/function.php';
+
+//enable session
 session_start();
+
 if (isset($_SESSION["admin"])){
-$sql_query_check_token = "SELECT token from user WHERE token = '{$_SESSION["admin"]}';";
-$sql_qu_tok_result = mysqli_query($db_con, $sql_query_check_token);
-$sql_qu_tok_result_num = mysqli_num_rows($sql_qu_tok_result);
-if ($sql_qu_tok_result_num > 0){
-    while ($row = mysqli_fetch_assoc($sql_qu_tok_result)){
-        if ($row["token"] === $_SESSION["admin"]){
-            echo "You are Admin";
-        }else{
-            header("Location: /");
-        }
-    }
-}
+	//get session
+	$session = $_SESSION["admin"];
+
+	//session checker
+	check_session($session);
+	
+	if ($is_admin === TRUE){
+		//echo "<p style=\"text-align:center; background-color: greenyellow;\">Admin</p>";
+	}else{
+		header("Location: /");
+	}
 }else{
     header("Location: /");
 }
 
 ?>
-
-
-<?php //include "db_con.php"; // include database connection ?>
 <?php
 if (isset($_GET["goods_name"])) {
 	$goods_name = $_GET["goods_name"];
@@ -44,36 +49,9 @@ if (isset($_GET["goods_name"])) {
 
         $db_con -> close();
 }
-
- ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Insert Data</title>
-</head>
-<body>
-	<center>
-		<form method="_GET" action="" >
-			<table>
-				<tr>
-					<td>Name :</td>
-					<td><input name="goods_name" type="text"></td>
-				</tr>
-				<tr>
-					<td>Price:</td>
-					<td><input name="goods_price" type="text"></td>
-				</tr>
-				<tr>
-					<td>Unit :</td>
-					<td><input name="goods_unit" type="text"></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type="submit" value="Submit"></td>
-				</tr>
-			</table>
-		</form>
-	</center>
-</body>
-</html>
+?>
+<?php include 'include/layout/htmlstart.php'; ?>
+<?php include 'include/layout/header.php';?>
+<?php include 'include/layout/insert-form.php'; ?>
+<?php include 'include/layout/footer.php';?>
+<?php include 'include/layout/htmlend.php';?>
---
+++ include/layout/insert-form.php
@@ -0,0 +1,23 @@
+<center>
+		<form method="_GET" action="" >
+			<table>
+				<tr>
+					<td>Name :</td>
+					<td><input name="goods_name" type="text"></td>
+				</tr>
+				<tr>
+					<td>Price:</td>
+					<td><input name="goods_price" type="text"></td>
+				</tr>
+				<tr>
+					<td>Unit :</td>
+					<td><input name="goods_unit" type="text"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Submit"></td>
+				</tr>
+			</table>
+		</form>
+		<?php include 'include/layout/global_link.php'; ?>
+</center>
